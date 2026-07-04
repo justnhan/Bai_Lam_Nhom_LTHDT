7bda23541dc52194a7b4c10f799964944395bafb
hoanhnee <mam048513@gmail.com>
2026-07-04 21:31:06 +0700

    dinh dang

--- WindowsFormsApp6/WindowsFormsApp6/DAL/ILichHenDAL.cs
+++ WindowsFormsApp6/WindowsFormsApp6/DAL/ILichHenDAL.cs
@@ -5,9 +5,13 @@ namespace Bai_Lam_Nhom_LTHDT.DAL
 {
     public interface ILichHenDAL
     {
+        bool ExistsMaHen(string maHen);
         List<LichHen> GetAllLichHen();
+        List<LichHen> SearchByMaChuyenKhoa(string maChuyenKhoa);
+        List<LichHen> SearchByMaBacSi(string maBacSi);
 
         LichHen GetByMaHen(string maHen);
+        string GetHoTenBSByMaHen(string maHen);
 
         bool Add(LichHen lichHen);
 
@@ -16,5 +20,12 @@ namespace Bai_Lam_Nhom_LTHDT.DAL
         bool DeleteByMaHen(string maHen);
 
         string GetError();
+        List<ThongTinLichHen> GetDanhSachThongKe();
+        Dictionary<string, int> GetThongKeTheoThu();
+        List<CanhBao> GetDanhSachCanhBao();
+        int GetTongQuaGio();
+        int GetTongCanhBaoKhan();
+        int TongCanhBao();
+        Dictionary<string, int> GetThongKeTrangThai();
     }
 }
